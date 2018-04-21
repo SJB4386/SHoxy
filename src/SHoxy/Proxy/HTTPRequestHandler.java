5e600548f470283f0d12ca146e0e3dc77bcc47b2
William Ray Johnson <willrayjohnson@gmail.com>
2018-04-20 23:02:11 -0500

    Set up request decoding

--- src/SHoxy/Proxy/HTTPRequestHandler.java
+++ src/SHoxy/Proxy/HTTPRequestHandler.java
@@ -6,13 +6,16 @@ import java.util.Map;
 
 import SHoxy.Cache.CachedItem;
 import SHoxy.HTTP.HTTPData;
+import SHoxy.HTTP.HTTPEncoderDecoder;
 import SHoxy.Util.DocRetriever;
 
 public class HTTPRequestHandler implements Runnable {
     private final static String ERROR_501_FILENAME = "HTTPErrorDocs/501.html";
+    private final static int REQUEST_BUFFER_SIZE = 800;
 
     private Socket clientSocket;
     private OutputStream replyStream;
+    private InputStream requestStream;
 
     public HTTPRequestHandler(Socket clientSocket, Map<String, CachedItem> cache,
             String cacheDirectory) {
@@ -21,10 +24,26 @@ public class HTTPRequestHandler implements Runnable {
 
     @Override
     public void run() {
+        byte[] requestBuffer = new byte[REQUEST_BUFFER_SIZE];
+        byte[] rawRequest;
+        int requestSize;
+        HTTPData clientRequest;
+
         try {
             System.out.printf("Client connected on port: %d\n", clientSocket.getPort());
+
+            requestStream = clientSocket.getInputStream();
             replyStream = clientSocket.getOutputStream();
+            requestSize = requestStream.read(requestBuffer);
+            if (requestSize > 0) {
+                rawRequest = new byte[requestSize];
+                System.arraycopy(requestBuffer, 0, rawRequest, 0, requestSize);
+                clientRequest = HTTPEncoderDecoder.decodeMessage(rawRequest);
+                System.out.println(clientRequest.toString());
+            }
+
             replyStream.write(get501Packet());
+            requestStream.close();
             replyStream.close();
         } catch (IOException e) {
             System.out.println("Error connecting to client");
