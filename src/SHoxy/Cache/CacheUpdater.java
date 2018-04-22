0a815c33cacca8ee44ec2b9acc514d40dece0f98
SJB4386 <33796160+SJB4386@users.noreply.github.com>
2018-04-22 16:03:18 -0500

    Cache updater update
    When updating the cache, for each cache item, it tries to get the lock, and if successful, should send if-modified-since request, and handle the result.

--- src/SHoxy/Cache/CacheUpdater.java
+++ src/SHoxy/Cache/CacheUpdater.java
@@ -1,8 +1,18 @@
 package SHoxy.Cache;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+
+import SHoxy.HTTP.HTTPData;
+import SHoxy.Proxy.SHoxyProxy;
+import SHoxy.Util.SHoxyUtils;
 
 public class CacheUpdater implements Runnable {
 
@@ -45,9 +55,60 @@ public class CacheUpdater implements Runnable {
     }
 
     private void updateCachedItem(CachedItem item) {
-        // TODO Get lock on item. If file at URL has been modified, get item from URL, write file to disk
+        Lock lock = item.getLock();
+        URL destination;
+        HttpURLConnection connection = null;
+        BufferedReader responseBuffer;
+        StringBuffer rawResponse;
+        String responseLine;
+        int responseCode;
+        HTTPData response = null;
+        
+        if (lock.tryLock()) {
+            try {
+                destination = new URL(item.URL);
+                connection = (HttpURLConnection) destination.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setIfModifiedSince(item.lastModified.getTime());
+                responseCode = connection.getResponseCode();
+                
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    response = new HTTPData();
+                    response.isReply = true;
+                    response.protocol = SHoxyProxy.HTTP_VERSION;
+                    response.statusCode = "200 OK\r\n";
+                    response.responseCode = responseCode;
+                    responseBuffer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    rawResponse = new StringBuffer();
+                    while ((responseLine = responseBuffer.readLine()) != null)
+                        rawResponse.append(responseLine + "\n");
+                    responseBuffer.close();
+                    response.body = rawResponse.toString().getBytes();
+                    response.headerLines.put("Content-Length:", String.format("%d\r\n", response.body.length));
+                    
+                    writeFile(item.URL, response.body);
+                }
+                // if (responseCode == HttpURLConnection.HTTP_NOT_MODIFIED) or anything else, do nothing
+            } catch (MalformedURLException e) {
+                SHoxyUtils.logMessage(String.format("URL: %s not formatted properly", item.URL));
+            } catch (IOException e) {
+                SHoxyUtils.logMessage(String.format("Couldn't reach url %s", item.URL));
+            }
+            
+            lock.unlock();
+        }
+    }
+        
+    private void writeFile(String url, byte[] contents) {
+        FileOutputStream file = null;
+        try {
+            file = new FileOutputStream(url);
+            file.write(contents);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            SHoxyUtils.logMessage(String.format("Failed to write file to %s", url));
+        }
         
     }
-    
-
 }
