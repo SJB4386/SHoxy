a26a59d3b6805df1898c26b37e6eaa132e9400df
SJB4386 <33796160+SJB4386@users.noreply.github.com>
2018-04-22 18:24:39 -0500

    Organized imports and unused variables in Util

--- src/SHoxy/Util/SHoxyUtils.java
+++ src/SHoxy/Util/SHoxyUtils.java
@@ -1,14 +1,14 @@
 package SHoxy.Util;
 
-import java.io.*;
-import java.net.URI;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import SHoxy.Cache.CachedItem;
 
 public class SHoxyUtils {
     private final static String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
