5147ad3c1a27642eae246a51d83b6166a9e79f52
SJB4386 <33796160+SJB4386@users.noreply.github.com>
2018-04-22 16:30:47 -0500

    Wrote a config reader in main

--- src/SHoxy/Proxy/SHoxyProxy.java
+++ src/SHoxy/Proxy/SHoxyProxy.java
@@ -1,13 +1,17 @@
 package SHoxy.Proxy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 import SHoxy.Cache.*;
+import SHoxy.Util.SHoxyUtils;
 
 public class SHoxyProxy {
     public static final String HTTP_VERSION = "HTTP/1.1";
 
     private static final String CONFIG_FILE = "SHoxyProxy.config";
+    
 
     public static void main(String[] args) {
         Map<String, CachedItem> cache = new LinkedHashMap<String, CachedItem>();
@@ -19,9 +23,40 @@ public class SHoxyProxy {
         int updateTimerSeconds = 10;
         int deleteTimerSeconds = 1000;
         String cacheDirectory = "cache";
-        // TODO Load config and send values to threads
-
         
+        
+        String[] configValues = {"Listening-Port:", "Update-Timer:",
+                                 "Deletion-Timer:", "Cache-Directory:"};
+        
+        // Load config and send values to threads
+        
+        try {
+            Scanner configReader;
+            configReader = new Scanner(new File(CONFIG_FILE));
+            while (configReader.hasNextLine()) {
+                String line = configReader.nextLine();
+                for (int i = 0; i < configValues.length; i++) {
+                    if (line.startsWith(configValues[i])) {
+                        String lineValue = line.substring(configValues[i].length()).trim();
+                        if (i == 0) {
+                            listenerPort = Integer.parseInt(lineValue);
+                        } else if (i == 1) {
+                            updateTimerSeconds = Integer.parseInt(lineValue);
+                        } else if (i == 2) {
+                            deleteTimerSeconds = Integer.parseInt(lineValue);
+                        } else if (i == 3) {
+                            cacheDirectory = lineValue;
+                        }
+                        break;
+                    }
+                }
+                
+            }
+            configReader.close();
+        } catch (FileNotFoundException e) {
+            SHoxyUtils.logMessage("Config file not found.");
+        }
+
         Thread listenerThread = new Thread(new TCPListener(listenerPort, cache, cacheDirectory));
         Thread updaterThread = new Thread(new CacheUpdater(cache, updateTimerSeconds));
         Thread cleanerThread = new Thread(new CacheCleaner(cache, deleteTimerSeconds));
@@ -30,4 +65,6 @@ public class SHoxyProxy {
         updaterThread.start();
         cleanerThread.start();
     }
+    
+    
 }
